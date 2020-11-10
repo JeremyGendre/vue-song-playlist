127ae6d55b77f2540e7677dc406d509270f02bb3
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-10 22:04:23 +0100

    refactor router emplacmeent

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import router from './router';
+import router from './plugins/router';
 import PerfectScrollbar from 'vue2-perfect-scrollbar';
 import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css';
 import store from "./plugins/vuex";
