fc44b6f26589b9db04b2b423b3a7f9f3c660cf65
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-30 17:29:30 +0100

    handle fetch responses

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,9 +2,12 @@ self.addEventListener('install', event =>  {
     const preCache = async () => {
         const cache = await caches.open('static');
         return cache.addAll([
+            'index.html',
             '/images/background.jpg',
             '/images/default_song.png',
             '/images/profile.jpg',
+            'manifest.json',
+            'robots.txt',
             'icon-192x192.png',
             'icon-256x256.png',
             'icon-384x384.png',
@@ -31,11 +34,18 @@ self.addEventListener('activate', (event) => {
     )
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch',  event => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            console.log(response);
-            return response || fetch(event.request);
+        caches.open('dynamic').then(cache => {
+            return cache.match(event.request).then(response => {
+                console.log(response);
+                return response || fetch(event.request).then(response => {
+                    if(event.request.method.toLowerCase() === 'get'){
+                        cache.put(event.request, response.clone());
+                    }
+                    return response;
+                });
+            });
         })
     );
 });
